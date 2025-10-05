806eb2ce364c48dde00911d4f6d9b48e2095122d
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-10-04 21:42:51 -0400

    chore: installed nodemon and dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import { initDatabase } from './db/init.js'
 import { app } from './app.js'
-const PORT = 3000
-app.listen(PORT)
-console.info(`express server running on http://localhost:${PORT}`)
+try {
+  await initDatabase()
+  const PORT = process.env.PORT
+  app.listen(PORT)
+  console.info(`express server running on http://localhost:${PORT}`)
+} catch (err) {
+  console.error('error connecting to database:', err)
+}
