9982128a21dbd1c2e412d0a7b9e935ad9026758a
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-10-04 22:14:23 -0400

    chore: added postlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { Post } from './components/post.jsx'
+import { PostList } from './components/PostList.jsx'
+
+const posts = [
+  {
+    title: 'Full-Stack React Projects',
+    content: "Let's become full-stack developers!",
+    author: 'Daniel Bugl',
+    imageUrl:
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700,636',
+  },
+  {
+    title: 'Hello React!',
+    imageUrl:
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700,636',
+  },
+]
+
 export function App() {
-  return (
-    <Post
-      title='Full-Stack React Projects'
-      content="Let's become full-stack developers!"
-      author='Daniel Bugl'
-      imageUrl='https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=700,636'
-    />
-  )
+  return <PostList posts={posts} />
 }
