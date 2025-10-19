1a3c09e4332a2398f007e685a44dede79998aa16
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-10-19 17:00:18 -0400

    chore: corrected Post.jsx filename

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,6 +1,6 @@
 import { Fragment } from 'react'
 import PropTypes from 'prop-types'
-import { Post } from './post.jsx'
+import { Post } from './Post.jsx'
 export function PostList({ posts = [] }) {
   return (
     <div>
