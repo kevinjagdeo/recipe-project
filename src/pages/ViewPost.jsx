3a4e36e2232efffe3ed6ab74eb9c6c93729ae92c
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-10-20 23:27:07 -0400

    chore: improve social media embeds

--- src/pages/ViewPost.jsx
+++ src/pages/ViewPost.jsx
@@ -5,12 +5,20 @@ import { Header } from '../components/Header.jsx'
 import { Post } from '../components/Post.jsx'
 import { getPostById } from '../api/posts.js'
 import { Helmet } from 'react-helmet-async'
+import { getUserInfo } from '../api/users.js'
 export function ViewPost({ postId }) {
   const postQuery = useQuery({
     queryKey: ['post', postId],
     queryFn: () => getPostById(postId),
   })
   const post = postQuery.data
+  const userInfoQuery = useQuery({
+    queryKey: ['users', post?.author],
+    queryFn: () => getUserInfo(post?.author),
+    enabled: Boolean(post?.author),
+  })
+  const userInfo = userInfoQuery.data ?? {}
+
   function truncate(str, max = 160) {
     if (!str) return str
     if (str.length > max) {
@@ -26,6 +34,14 @@ export function ViewPost({ postId }) {
         <Helmet>
           <title>{post.title} | Full-Stack React Recipes</title>
           <meta name='description' content={truncate(post.contents)} />
+          <meta property='og:type' content='article' />
+          <meta property='og:title' content={post.title} />
+          <meta property='og:article:published_time' content={post.createdAt} />
+          <meta property='og:article:modified_time' content={post.updatedAt} />
+          <meta property='og:article:author' content={userInfo.username} />
+          {(post.tags ?? []).map((tag) => (
+            <meta key={tag} property='og:article:tag' content={tag} />
+          ))}
         </Helmet>
       )}
 
