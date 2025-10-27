2ac5b883829fa3e3f7f1018ab0b3c2ab3afa2d12
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-10-27 19:14:45 -0400

    feat: implement graphql

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,22 @@ import { userRoutes } from './routes/users.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import { eventRoutes } from './routes/events.js'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import { typeDefs, resolvers } from './graphql/index.js'
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
 
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
+apolloServer
+  .start()
+  .then(() => app.use('/graphql', expressMiddleware(apolloServer)))
+
 postsRoutes(app)
 userRoutes(app)
 eventRoutes(app)
