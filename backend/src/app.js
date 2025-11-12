97a984f5ab01eb3039289d6d6b89d520bd84d10e
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-11-11 22:32:14 -0500

    chore: create backend for a chat app using socket.io

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,29 +5,27 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import { createServer } from 'node:http'
 import { Server } from 'socket.io'
-
+import { handleSocket } from './socket.js'
 const app = express()
-
-app.use(cors())
 app.use(bodyParser.json())
+app.use(cors())
 postsRoutes(app)
 userRoutes(app)
 app.get('/', (req, res) => {
-  res.send('Hello from Express Live!')
+  res.send('Hello from Express Nodemon!')
 })
-
 const server = createServer(app)
 const io = new Server(server, {
   cors: {
     origin: '*',
   },
 })
-io.on('connection', (socket) => {
+handleSocket(io)
+/* io.on('connection', (socket) => {
   console.log('user connected:', socket.id)
   socket.on('disconnect', () => {
     console.log('user disconnected:', socket.id)
   })
-})
+}) */
 export { server as app }
-
-//export { app }
+// export { app }
