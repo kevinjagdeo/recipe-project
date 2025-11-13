e9a40303570a6f77c74ef993dcb28f604c9943da
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-11-12 22:07:52 -0500

    feat: added authentication with JWT

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import { io } from 'socket.io-client'
 
 const socket = io(import.meta.env.VITE_SOCKET_HOST, {
   query: 'room=' + new URLSearchParams(window.location.search).get('room'),
+  auth: {
+    token: new URLSearchParams(window.location.search).get('token'),
+  },
 })
 const queryClient = new QueryClient()
 const router = createBrowserRouter([
