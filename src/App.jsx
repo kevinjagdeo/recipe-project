959c5a23fe6bc7b71742a120e1dc2fa46f77c206
Kevin Jagdeo <70348657+kevinjagdeo@users.noreply.github.com>
2025-11-12 23:08:40 -0500

    chore: fix posts on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,14 @@ import { Signup } from './pages/Signup.jsx'
 import { Login } from './pages/Login.jsx'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-// import { io } from 'socket.io-client'
-import { SocketIOContextProvider } from './contexts/SocketIOContext.jsx'
+import { io } from 'socket.io-client'
 
-/* const socket = io(import.meta.env.VITE_SOCKET_HOST, {
+const socket = io(import.meta.env.VITE_SOCKET_HOST, {
   query: 'room=' + new URLSearchParams(window.location.search).get('room'),
   auth: {
     token: new URLSearchParams(window.location.search).get('token'),
   },
-}) */
-
+})
 const queryClient = new QueryClient()
 const router = createBrowserRouter([
   {
@@ -30,11 +28,11 @@ const router = createBrowserRouter([
   },
 ])
 
-/* socket.on('connect', async () => {
+socket.on('connect', async () => {
   console.log('connected to socket.io as', socket.id)
   socket.emit(
     'chat.message',
-    new URLSearchParams(window.location.search).get('mymessage'),
+    new URLSearchParams(window.location.search).get('mymsg'),
   )
   const userInfo = await socket.emitWithAck('user.info', socket.id)
   console.log('user info', userInfo)
@@ -44,15 +42,13 @@ socket.on('connect_error', (err) => {
 })
 socket.on('chat.message', (msg) => {
   console.log(`${msg.username}: ${msg.message}`)
-}) */
+})
 
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
-        <SocketIOContextProvider>
-          <RouterProvider router={router} />
-        </SocketIOContextProvider>
+        <RouterProvider router={router} />
       </AuthContextProvider>
     </QueryClientProvider>
   )
